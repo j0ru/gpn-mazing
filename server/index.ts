606ec26893a31af064f4f635c70366db9683bb7b
Folke H. Gleumes <folke@gleumes.org>
2022-05-23 20:05:33 +0200

    add port

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import { MazeServer } from "./MazeServer"
 
 const GAME_PORT = parseInt(process.env.GAME_PORT || '') || 4000
-const VIEW_PORT = parseInt(process.env.VIEW_PORT || '') || 4001
+const VIEW_PORT = parseInt(process.env.VIEW_PORT || '') || 5005
 
 const mazeServer = new MazeServer(GAME_PORT, VIEW_PORT)
 
